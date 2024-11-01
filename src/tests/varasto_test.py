9ee050c9434a6f13938db69fbc5e1be0249b8403
Jaakko Airikkala <jaakkel@gmail.com>
2024-11-01 16:55:00 +0200

    Fix test

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -8,7 +8,6 @@ class TestVarasto(unittest.TestCase):
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
-        raise RuntimeError("Oho")
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_uudella_varastolla_oikea_tilavuus(self):
